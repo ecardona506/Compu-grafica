ed1e801881a7679a223764b85ed2818c0103d3af
ecardona506 <ecardona506@javerianacali.edu.co>
2020-10-01 23:39:07 -0500

    Clase Proyector

---
+++ Proyector.cpp
@@ -0,0 +1,24 @@
+#include "Proyector.h"
+
+void Proyector::abrirMalla() {
+    this->objmodel_ptr = NULL;
+    if (!this->objmodel_ptr)
+    {
+        this->objmodel_ptr = glmReadOBJ("./modelos/holo-table.obj");
+        if (!objmodel_ptr)
+            exit(0);
+        glmUnitize(this->objmodel_ptr);
+        glmFacetNormals(this->objmodel_ptr);
+        glmVertexNormals(this->objmodel_ptr, 90.0);
+    }
+}
+
+void Proyector::dibujarMalla(float x, float y, float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    glPushMatrix();
+    glTranslatef(x, y, z);
+    glmDraw(objmodel_ptr, GLM_SMOOTH | GLM_MATERIAL);
+    glPopMatrix();
+}
---
+++ Proyector.h
@@ -0,0 +1,13 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+#include "glm.h"
+
+class Proyector
+{
+	float x, y, z;
+	GLMmodel* objmodel_ptr;
+public:
+	void abrirMalla();
+	void dibujarMalla(float x, float y, float z);
+};
