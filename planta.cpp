b8ccac24ffd9861385b22cb0485c9f734dcbd18a
ecardona506 <ecardona506@javerianacali.edu.co>
2020-11-03 01:08:56 -0500

    Planta con colores

--- planta.cpp
+++ planta.cpp
@@ -15,6 +15,14 @@ void planta::abrirMalla() {
 }
 void planta::dibujarMalla(float x, float y, float z) {
     glPushMatrix();
+    // Material parameters:
+
+     GLfloat material_Ka[] = {0.5f, 0.0f, 0.0f, 1.0f};
+     GLfloat material_Kd[] = {0.4f, 0.4f, 0.5f, 1.0f};
+     GLfloat material_Ks[] = {0.8f, 0.8f, 0.0f, 1.0f};
+     GLfloat material_Ke[] = {0.1f, 0.0f, 0.0f, 0.0f};
+     GLfloat material_Se = 20.0f;
+
     glTranslatef(x, y, z);
     glmDraw(objmodel_ptr, GLM_SMOOTH | GLM_MATERIAL);
     glPopMatrix();
